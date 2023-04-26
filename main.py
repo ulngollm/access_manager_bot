ee3620def49756f374ec70251f8c5e604252d6db
ulngollm <ulngollm@gmail.com>
2023-04-26 21:02:59 +0300

    упрощено взаимодействие с пользователем

--- main.py
+++ main.py
@@ -16,21 +16,20 @@ app = Client('bot', API_ID, API_HASH, bot_token=BOT_API_TOKEN)
 access_list = {}
 
 class AccessStatus:
-    ALLOWED = 0
-    WAIT = 1
+    ALLOWED = 1
     DENIED = 2
 
 
 def check_access(func):
     def wrapper(client, message):
         if access_list.get(message.from_user.id) != AccessStatus.ALLOWED:
-            request_access(client, message)
+            notify_access_limit(client, message)
             return
         func(client, message)
     return wrapper
 
 
-def request_access(client: Client, message: Message):
+def request_access(client: Client, callback_query: CallbackQuery):
     client.send_message(
         random.choice(ADMIN_LIST),
         "Пользователь запрашивает доступ",
@@ -38,33 +37,36 @@ def request_access(client: Client, message: Message):
             [
                 InlineKeyboardButton(
                     "Просмотреть профиль",
-                    user_id=message.from_user.id
+                    user_id=callback_query.from_user.id
                 )
             ],
             [
                 InlineKeyboardButton(
                     "Принять",
-                    callback_data= "accept:%d" % message.from_user.id
+                    callback_data= "accept:%d" % callback_query.from_user.id
                 ),
                 InlineKeyboardButton(
                     "Отклонить",
-                    callback_data= "reject:%d" % message.from_user.id
+                    callback_data= "reject:%d" % callback_query.from_user.id
                 )
             ]
         ])
     )
 
-
-def notify_reject_user(client: Client, user_id):
-    client.send_message(
-        user_id,
-        "Извините, доступ запрещен."
+    callback_query.message.reply(
+        "Запрос отправлен модератору. Вам придет ответ в этом чате в ближайшее время."
     )
 
-def notify_accept_user(client: Client, user_id):
-    client.send_message(
-        user_id,
-        "Доступ разрешен."
+
+def notify_access_limit(client: Client, message: Message):
+    message.reply(
+        "Привет! \nИзвините, у нас тут закрытый клуб. Если хотите запросить приглашение, нажмите на кнопку.",
+        reply_markup=InlineKeyboardMarkup([[
+            InlineKeyboardButton(
+                "Пустите меня, пожалуйста",
+                callback_data="request"
+            )
+        ]])
     )
 
 
@@ -75,12 +77,24 @@ def deny_access(client: Client, callback_query: CallbackQuery):
     notify_reject_user(client, user_id)
 
 
+def notify_reject_user(client: Client, user_id):
+    client.send_message(
+        user_id,
+        "Извините, ваш запрос был отклонен. Попробуйте позднее."
+    )
+
 def allow_access(client: Client, callback_query: CallbackQuery):
     user_id = int(callback_query.matches[0].group(1))
     access_list[user_id] = AccessStatus.ALLOWED
     callback_query.answer("Доступ для пользователя %d разрешен" % user_id, show_alert=True)
     notify_accept_user(client, user_id)
 
+
+def notify_accept_user(client: Client, user_id):
+    client.send_message(
+        user_id,
+        "Привет! Можешь пользоваться ботом."
+    )
 
 
 @check_access
@@ -89,7 +103,7 @@ def reply_message(client: Client, message: Message):
 
 
 @check_access
-def hello(client, message):
+def hello(client: Client, message: Message):
     message.reply(
         'Привет!'
     )
@@ -97,6 +111,7 @@ def hello(client, message):
 app.add_handler(MessageHandler(hello, filters.command(['start'])))
 
 app.add_handler(MessageHandler(reply_message, filters.text))
+app.add_handler(CallbackQueryHandler(request_access, filters.regex(pattern='request')))
 app.add_handler(CallbackQueryHandler(deny_access, filters.regex(pattern='reject:(\d*)')))
 app.add_handler(CallbackQueryHandler(allow_access, filters.regex(pattern='accept:(\d*)')))
 
